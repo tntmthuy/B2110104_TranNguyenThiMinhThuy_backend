0ac2615144598be3e7e346040b50fe6d25928f24
Minh Thuy <trannguyenminhthuyag@gmail.com>
2024-03-06 14:37:39 +0700

    Dinh nghĩa cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const cors = require("cors");
 
 const app = express();
 
+//route
+const contactsRouter = require("./app/routes/contact.route");
+
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", contactsRouter); //route
 
 app.get("/", (req, res) => {
 	res.json({ message: "Welcome to contact book application." });
