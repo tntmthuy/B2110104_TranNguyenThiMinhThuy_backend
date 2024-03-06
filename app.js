286608fb3a34dd8104e4119e3d41e06dc7cc1fea
Minh Thuy <trannguyenminhthuyag@gmail.com>
2024-03-06 20:09:30 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
 const express = require("express");
 const cors = require("cors");
 
-const app = express();
-
 //route
 const contactsRouter = require("./app/routes/contact.route");
 
+//api-error
+const ApiError = require("./app/api-error");
+
+const app = express();
+
 app.use(cors());
 app.use(express.json());
-app.use("/api/contacts", contactsRouter); //route
 
 app.get("/", (req, res) => {
-	res.json({ message: "Welcome to contact book application." });
+	res.json({ 
+	message: "Welcome to contact book application." 
+	});
+});
+
+app.use("/api/contacts", contactsRouter); //route
+
+//handle 404 response
+app.use((req, res, next) => {
+	return next(new ApiError(404, "Resource not found"));
+});
+
+//define error-handling middleware last, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+	return res.status(err.statusCode || 500).json({
+		message: err.message || "Internal Server Error",
+	});
 });
 
 module.exports = app;
